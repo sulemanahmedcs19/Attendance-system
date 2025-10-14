9604c8a8391f7715934e1ebdb1f466f7f2e11dff
Suleman Ahmed <sulemanahmed1077@gmail.com>
2025-10-14 02:17:20 -0700

    changes

--- Client/src/Components/Dashboard.jsx
+++ Client/src/Components/Dashboard.jsx
@@ -1,5 +1,4 @@
 import Header from './Header';
-<<<<<<< HEAD:src/Components/Dashboard.jsx
 import Sidebar  from './Sidebar';
 import Card from './Card';
 import Graph from './Graph';
@@ -21,32 +20,3 @@ function Dashboard() {
 }
 
 export default Dashboard;
-=======
-import Sidebar from './Sidebar';
-import Card from './Card';
-import Graph from './Graph';
-import React from 'react';  
-
-function Dashboard() 
-{
-    return(
-
-
-        <>
-        {/* <div className="App flex bg-gray-100"> */}
-        <Sidebar />
-        <div className="ml-[20%] w-[80%] flex flex-col mt-[2%]">
-          <Header />
-            <Card />
-            <Graph />
-          </div>
-      {/* </div> */}
-        
-        
-        </>
-    )
-
-  }
-
-  export default Dashboard;
->>>>>>> 3a79fa249f534802c090aabd145bcaa6921a3558:Client/src/Components/Dashboard.jsx
--- Client/src/Components/Employe.jsx
+++ Client/src/Components/Employe.jsx
@@ -1,173 +1,106 @@
 import React from 'react';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import { Link } from 'react-router-dom';
 
 function Employe() {
   return (
-  <>
-   
-   <Header/>
-   <Sidebar />
-    <div className="max-w-5xl mx-auto p-6 bg-white shadow-lg rounded-lg mt-30">
+    <div className="max-w-7xl mx-auto p-6 bg-white shadow-lg rounded-lg">
       <form>
-        
-        <h2 className="text-2xl font-semibold text-gray-900 mb-6">Basic Information</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Full Name</label>
-            <input
-              type="text"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-            />
+       
+        <h1 className="text-2xl font-semibold text-gray-800 mb-4">Basic Information</h1>
+        <div className="flex flex-wrap gap-6 mb-6">
+          <div className="w-85 ">
+            <label className=" text-gray-700">Full Name</label>
+            <input type="text" className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
           </div>
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Date of Birth</label>
+          <div className="w-85 ">
+            <label className=" text-gray-700">Date of Birth</label>
             <input
-              type="date"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-            />
+              type="date" className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
           </div>
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">CNIC / NIC Number</label>
-            <input
-              type="text"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-            />
+
+          <div className="w-85">
+            <label className=" text-gray-700">CNIC Number</label>
+            <input type="text" className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
           </div>
+          <div className="w-85">
+            <label className=" text-gray-700">Gender</label>
+            <select className="mt-1 p-2 w-full border border-gray-300 rounded-md">
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+
+            <div className="w-85">
+            <label className=" text-gray-700">Upload CNIC Photo</label>
+            <select className="mt-1 p-2 w-full border border-gray-300 rounded-md">
+              <option value="Male">Front</option>
+              <option value="Female">Bck</option>
+              
+            </select>
+          </div>
+
+
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mb-8">
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Gender</label>
-            <select
-              className="mt-1 p-2 w-full border border-purple-600 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 bg-white text-gray-900"
-              defaultValue=""
-            >
-              <option  >
-                Select Gender
-              </option>
-              <option >Male</option>
-              <option >Female</option>
-              <option >Other</option>
-            </select>
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">Contact Details</h2>
+        <div className="flex flex-wrap gap-6 mb-6">
+          <div className="w-85">
+            <label className=" text-gray-700">Home Address</label>
+            <input type="text" className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
           </div>
-
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Upload CNIC Photo</label>
-            <select
-              className="mt-1 p-2 w-full border border-purple-600 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 bg-white text-gray-900"
-              defaultValue=""
-            >
-              <option >
-                Select Side
-              </option>
-              <option>Front</option>
-              <option>Back</option>
-            </select>
+          <div className="w-85">
+            <label className=" text-gray-700">City</label>
+            <input type="text" className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
+          
+          </div>
+          <div className="w-85">
+            <label className=" text-gray-700">Country</label>
+            <input type="text" className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
+            
+          </div>
+          <div className="w-85">
+            <label className=" text-gray-700">Personal Email</label>
+            <input type="email" className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
+          </div>
+          <div className="w-85">
+            <label className=" text-gray-700">Emergency Contact</label>
+            <input type="text" className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
           </div>
         </div>
 
        
-        <h2 className="text-2xl font-semibold text-gray-900 mb-6">Contact Details</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Home Address</label>
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">Roles & Work Details</h2>
+        <div className="flex flex-wrap gap-6 mb-6">
+          <div className="w-85">
+            <label className=" text-gray-700">Job Title</label>
+            <input type="text" className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
+          </div>
+          <div className="w-85">
+            <label className=" text-gray-700">Reporting Manager</label>
             <input
-              type="text"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-            />
+              type="text"className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
           </div>
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">City</label>
+          <div className="w-85">
+            <label className=" text-gray-700">Date of Joining</label>
             <input
-              type="text"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-            />
+              type="date" className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
           </div>
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Country</label>
+          <div className="w-85">
+            <label className=" text-gray-700">Department</label>
             <input
-              type="text"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-            />
+              type="text"className="mt-1 p-2 w-full border border-gray-300 rounded-md"/>
           </div>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-10">
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Personal Email</label>
-            <input
-              type="email"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-            />
-          </div>
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Emergency Contact</label>
-            <input
-              type="text"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-            />
-          </div>
-        </div>
-
-        <h2 className="text-2xl font-semibold text-gray-900 mb-6">Roles & Work Details</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Job Title</label>
-            <input
-              type="text"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-            />
-          </div>
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Reporting Manager</label>
-            <input
-              type="text"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-            />
-          </div>
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Date of Joining</label>
-            <input
-              type="date"
-              className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
-            />
-          </div>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Department</label>
-            <select
-              className="mt-1 p-2 w-full border border-purple-600 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 bg-white text-gray-900"
-              defaultValue=""
-            >
-              <option disabled value="">
-                Select Department
-              </option>
-              <option>Developer</option>
-              <option>Designer</option>
-              <option>Sales</option>
-              <option>Marketing</option>
-              <option>Other</option>
-            </select>
-          </div>
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Work Type</label>
-            <select
-              className="mt-1 p-2 w-full border border-purple-600 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600 bg-white text-gray-900"
-              defaultValue=""
-            >
-              <option disabled value="">
-                Select Work Type
-              </option>
-              <option>Onsite</option>
-              <option>Remote</option>
-              <option>Hybrid</option>
+          <div className="w-85">
+            <label className=" text-gray-700">Work Type</label>
+            <select className="mt-1 p-2 w-full border border-gray-300 rounded-md">
+              <option value="Onsite">Onsite</option>
+              <option value="Remote">Remote</option>
+              <option value="Hybrid">Hybrid</option>
             </select>
           </div>
         </div>
       </form>
     </div>
-    </>
   );
 }
 
